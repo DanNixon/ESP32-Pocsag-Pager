6f69bb82397badbfd6a490630d519cc6f31494cf
Dan Nixon <dan@dan-nixon.com>
2024-05-15 22:02:34 +0100

    Move pin off voltage detection

--- Arduino Sketch/src/periph.h
+++ Arduino Sketch/src/periph.h
@@ -3,7 +3,7 @@
 #define LORA_MOSI       27
 #define LORA_SS         18
 #define LORA_DIO0       26
-#define LORA_DIO1       35
+#define LORA_DIO1       12
 #define LORA_DIO2       34
 #define LORA_RST        23
 
